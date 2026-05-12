8576e4c322e1d458c1ac8aef278ca0a050d7a9c2
molang404 <art104233@naver.com>
2026-05-12 12:51:09 +0900

    에러 수정

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,22 +4,22 @@ import { Prisma } from "../generated/prisma/client.ts";
 
 const createUser = async (data: UserCreateInput) => {
     try {
-        return prisma.user.create({
+        return await prisma.user.create({
             data,
         });
         // 에러 Prisma
     } catch (error) {
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
             if (error.code === "P2002") {
-                const target = error.meta?.target as string[];
+                const errorMassage = error.message;
 
-                if (target?.includes("username")) {
+                if (errorMassage.includes("username")) {
                     throw new Error("ALREADY_EXISTS_USERNAME");
                 }
-                if (target?.includes("email")) {
+                if (errorMassage.includes("email")) {
                     throw new Error("ALREADY_EXISTS_EMAIL");
                 }
-                if (target?.includes("nickname")) {
+                if (errorMassage.includes("nickname")) {
                     throw new Error("ALREADY_EXISTS_NICKNAME");
                 }
                 throw new Error("UNKNOWN_ERROR");
