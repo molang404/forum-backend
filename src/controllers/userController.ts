64c4b37f8d7705c7bfac0f3434a85c7eb2a16e9b
molang404 <art104233@naver.com>
2026-05-15 12:49:05 +0900

    작성 중

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,26 +4,43 @@ import userService from "../services/userService.ts";
 
 const createUser = async (req: Request, res: Response) => {
     try {
-    const { username, password, name, nickname, email, phoneNumber, birthdate, gender, role } = req.body;
+        const { username, password, name, nickname, email, phoneNumber, birthdate, gender, role } =
+            req.body;
 
-    const userData: UserCreateInput = {
-        username,
-        password,
-        name,
-        nickname,
-        email,
-        phoneNumber,
-        birthdate: birthdate ? new Date(birthdate) : null,
-        gender,
-        role,
-    }
+        const userData: UserCreateInput = {
+            username,
+            password,
+            name,
+            nickname,
+            email,
+            phoneNumber,
+            birthdate: birthdate ? new Date(birthdate) : null,
+            gender,
+            role,
+        };
 
-    const newUser: UserCreateInput = await userService.createUser(userData);
+        const newUser: UserCreateInput = await userService.createUser(userData);
 
-    res.status(201).json(newUser);
-
+        res.status(201).json(newUser);
     } catch (error) {
+        if (error instanceof Error) {
+            switch (error.message) {
+                case "ALREADY_EXISTS_USERNAME":
+                    res.status(409).json({ message: "이미 사용 중인 아이디입니다." });
+                    return;
+                case "ALREADY_EXISTS_EMAIL":
+                    res.status(409).json({ message: "이미 가입 된 이메일입니다."});
+                    return;
+                case "ALREADY_EXISTS_NICKNAME":
+                    res.status(409).json({ message: "이미 사용 중인 닉네임입니다."});
+                    return;
+                default: console.log(error);
+                    res.status(500).json({ message: "유저 생성 중 오류가 발생했습니다."});
+            }
+        }
+
         console.log(error);
+        res.status(500).json({ message: "유저 생성 중 오류가 발생했습니다."})
     }
 };
 
